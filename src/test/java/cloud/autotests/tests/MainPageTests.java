f50e3d19b7032a00c4a4bef2301c1ebfc4e8e089
Daniil Glushenkov <glushenkov.daniel@yandex.ru>
2022-10-02 23:24:37 +0300

    added tests click.ru

--- src/test/java/cloud/autotests/tests/MainPageTests.java
+++ src/test/java/cloud/autotests/tests/MainPageTests.java
@@ -2,9 +2,11 @@ package cloud.autotests.tests;
 
 import cloud.autotests.helpers.DriverUtils;
 import io.qameta.allure.Description;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
@@ -13,26 +15,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class MainPageTests extends TestBase {
     @Test
-    @DisplayName("Login modal form should appear on main page")
-    void generatedTest() {
-        step("Open url 'https://habr.com/'", () ->
-                open("https://habr.com/"));
+    @DisplayName("Vacancy form should appear after click the button")
+    void vacancyTest() {
+        step("Open url 'https://companyclick.ru'", () ->
+                open("https://companyclick.ru/#tabs_desc_415_6"));
 
-        step("Click on Profile button", () ->
-                $("[data-test-id=menu-toggle-guest]").click());
+        step("Click on button Vacancy", () ->
+                $("#menu-item-585").click());
 
-        step("Login modal should be visible", () ->
-                $(".tm-user-menu").shouldBe(visible));
+        step("Block Vacancy should be visible", () ->
+                $(".blog-postsrow").shouldBe(visible));
     }
 
     @Test
     @DisplayName("Page title should have header text")
     void titleTest() {
-        step("Open url 'https://habr.com/'", () ->
-                open("https://habr.com/"));
+        step("Open url 'https://companyclick.ru'", () ->
+                open("https://companyclick.ru/#tabs_desc_415_6"));
 
-        step("Page title should have text 'All posts in a row / Habr'", () -> {
-            String expectedTitle = "All posts in a row / Habr";
+        step("Page title should have text 'CLICK - affiliate services'", () -> {
+            String expectedTitle = "CLICK - affiliate services";
             String actualTitle = title();
 
             assertThat(actualTitle).isEqualTo(expectedTitle);
@@ -42,8 +44,8 @@ public class MainPageTests extends TestBase {
     @Test
     @DisplayName("Page console log should not have errors")
     void consoleShouldNotHaveErrorsTest() {
-        step("Open url 'https://habr.com/'", () ->
-                open("https://habr.com/"));
+        step("Open url 'https://companyclick.ru'", () ->
+                open("https://companyclick.ru/#tabs_desc_415_6"));
 
         step("Console logs should not contain text 'SEVERE'", () -> {
             String consoleLogs = DriverUtils.getConsoleLogs();
@@ -51,5 +53,18 @@ public class MainPageTests extends TestBase {
 
             assertThat(consoleLogs).doesNotContain(errorText);
         });
+    }
+
+    @Test
+    @DisplayName("Block Work Flow should not have text 'Cloud Databases'")
+    void javaWorkFlowTest() {
+        step("Open url 'https://companyclick.ru'", () ->
+                open("https://companyclick.ru/#tabs_desc_415_6"));
+
+        step("Click on button work flows", () ->
+                $("#menu-item-61").click());
+
+        step("Block Work Flow should not have text 'Cloud Databases'", () ->
+                $(".gridContainer").shouldNotHave(text("Cloud Databases")));
     }
 }
